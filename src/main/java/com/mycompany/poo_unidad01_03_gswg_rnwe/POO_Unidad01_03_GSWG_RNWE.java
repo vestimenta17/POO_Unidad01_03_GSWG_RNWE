135e40ecd614f0ba05244ade0d4ced7c79a4bb5c
HP <HP@DESKTOP-Q3VD0LT>
2022-04-22 05:48:45 -0500

    Practica3

---
+++ src/main/java/com/mycompany/poo_unidad01_03_gswg_rnwe/POO_Unidad01_03_GSWG_RNWE.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo_unidad01_03_gswg_rnwe;
+
+/**
+ *
+ * @author HP
+ */
+public class POO_Unidad01_03_GSWG_RNWE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
